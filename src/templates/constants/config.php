939bf040b11fc88c6dca3fbf9eab52a5f22a51b3
Takehiro Kato <czk@seeds-9.com>
2023-08-21 19:10:38 +0900

    add vite loader

---
+++ src/templates/constants/config.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * VITE開発環境かどうか
+ * @todo 環境変数に置き換える
+ */
+const IS_VITE_DEVELOPMENT = true;
--- src/templates/functions.php
+++ src/templates/functions.php
@@ -1,1 +1,4 @@
 <?php
+
+include_once "constants/config.php";
+include_once "utilities/viteLoader.php";
