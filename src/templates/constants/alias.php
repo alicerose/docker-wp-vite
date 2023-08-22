7a87f023dfb9801d5360aa6ed339afb81441fbda
Takehiro Kato <czk@seeds-9.com>
2023-08-22 19:24:43 +0900

    create alias

---
+++ src/templates/constants/alias.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * テンプレートディレクトリURIのエイリアス
+ */
+define("TEMPLATE_DIR_URI", get_template_directory_uri());
+
+/**
+ * アセットディレクトリURIのエイリアス
+ */
+define('ASSET_DIR_URI', get_template_directory_uri() . '/assets');
