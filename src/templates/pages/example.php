4c16f3bc749bf80b874a83d035558f0b0014402c
Takehiro Kato <czk@seeds-9.com>
2023-09-01 19:26:01 +0900

    add sample templates

---
+++ src/templates/pages/example.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template Name: サンプルテンプレート
+ * Template Post Type: page
+ */
+?>
+<?php get_header(); ?>
+<main>
+    <?php the_content(); ?>
+</main>
+<?php get_footer(); ?>
---
+++ src/templates/pages/example2.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template Name: サンプルテンプレート2
+ * Template Post Type: page
+ */
+?>
+<?php get_header(); ?>
+<main>
+    <?php the_content(); ?>
+</main>
+<?php get_footer(); ?>
